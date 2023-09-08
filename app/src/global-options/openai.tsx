52d45625fd44679a3d4b9f3902316d8d0a51aa3b
SamMM168 <43960960+SamMM168@users.noreply.github.com>
2023-09-08 08:56:56 +0800

    Update openai.tsx

--- app/src/global-options/openai.tsx
+++ app/src/global-options/openai.tsx
@@ -23,8 +23,11 @@ export const openAIOptions: OptionGroup = {
                         <FormattedMessage defaultMessage="全网最低价格GPT-4，1美元额度只需0.5元人民币" />
                     </p>
                     <p>
-                        <FormattedMessage defaultMessage="购买获得的卡密为本网站API Key，只能用在本网站。你的API Key只存在你本地设备，绝不会传输给任何人。
+                        <FormattedMessage defaultMessage="购买获得的卡密为本网站API Key，只能用在本网站或 https://nb.aitom.cc 。你的API Key只存在你本地设备，绝不会传输给任何人。
 " />
+                    </p>
+                    <p>
+                        <FormattedMessage defaultMessage="余额查询请前往 https://nb.aitom.cc ，输入API Key，点击余额查询" />
                     </p>
                 </>,
             }),
